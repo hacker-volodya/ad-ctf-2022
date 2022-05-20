8a32d36fe60650ee4dfc406ad5f74891f04ce55d
Vladimir Lebedev <d3fl4t3@gmail.com>
2022-05-20 21:29:10 +0700

    Enable scheduling

--- services/locator/backend/src/main/java/lebedev/locator/LocatorApplication.java
+++ services/locator/backend/src/main/java/lebedev/locator/LocatorApplication.java
@@ -2,10 +2,12 @@ package lebedev.locator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableScheduling
 public class LocatorApplication {
 
     public static void main(String[] args) {
